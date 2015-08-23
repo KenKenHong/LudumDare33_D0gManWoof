4b6834fdb377ba2600be00e7aa3253ab33c58903
KenKenHong <kenkenh94@gmail.com>
2015-08-23 01:42:48 -0500

    YoloBaggins
    Enemies at large! Jesus take the wheel.

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour
+{
+	public float fieldOfViewAngle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+
+	private NavMeshAgent nav;
+	private SphereCollider col;
+	private Animator anim;
+	//private LastPlayerSighting LastPlayerSighting;
+	private GameObject player;
+	private Animator playerAnim;
+	private Vector3 previousSighting;
+
+	void Start()
+	{
+		nav = GetComponent<NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
+        anim = GetComponent<Animator>();
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+        //player = GameObject.FindGameObjectWithTag(Tags.player);
+        playerAnim = player.GetComponent<Animator>();
+        //hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
+
+        //personalLastSighting = lastPlayerSighting.resetPosition;
+        //previousSighting = lastPlayerSighting.resetPosition;
+	}
+
+	void Update()
+	{
+		
+	}
+	
+}
---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+
+	public int caughtBool;
+	public int sneakingBool;
+	//public int playerInSightBool;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		caughtBool = Animator.StringToHash("Base Layer.Caught");
+		sneakingBool = Animator.StringToHash("Base Layer.Sneak");
+
+	}
+
+}
