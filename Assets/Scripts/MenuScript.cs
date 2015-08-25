2a1d8a327fbc1d4520ef087e76608dffc9a81218
James300 <jamesstewart94.300@gmail.com>
2015-08-24 20:24:40 -0500

    Map and UI
    Stuff

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -7,74 +7,19 @@ public class MenuScript : MonoBehaviour
 	public Canvas exitScreen;
 	public Canvas optionScreen;
 	public Canvas helpScreen;
-	public Button startButton;
-	public Button optionButton;
-	public Button helpButton;
-	public Button exitButton;
 
 	public void Start ()
 	{
-		exitScreen = exitScreen.GetComponent<Canvas>();
-		optionScreen = optionScreen.GetComponent<Canvas> ();
-		helpScreen = helpScreen.GetComponent<Canvas> ();
-		startButton = startButton.GetComponent<Button>();
-		optionButton = optionButton.GetComponent<Button>();
-		helpButton = helpButton.GetComponent<Button>();
-		exitButton = exitButton.GetComponent<Button>();
 		exitScreen.enabled = false;
 		optionScreen.enabled = false;
 		helpScreen.enabled = false;
-	}
-
-	public void optionPress()
-	{
-		optionScreen.enabled = true;
-		startButton.enabled = false;
-		optionButton.enabled = false;
-		helpButton.enabled = false;
-		exitButton.enabled = false;
-	}
-
-	public void xPress ()
-	{
-		optionScreen.enabled = false;
-		helpScreen.enabled = false;
-		startButton.enabled = true;
-		optionButton.enabled = true;
-		helpButton.enabled = true;
-		exitButton.enabled = true;
-	}
-
-	public void helpPress()
-	{
-		helpScreen.enabled = true;
-		startButton.enabled = false;
-		optionButton.enabled = false;
-		helpButton.enabled = false;
-		exitButton.enabled = false;
-	}
-
-	public void exitPress()
-	{
-		exitScreen.enabled = true;
-		startButton.enabled = false;
-		optionButton.enabled = false;
-		helpButton.enabled = false;
-		exitButton.enabled = false;
-	}
-
-	public void noPress()
-	{
-		exitScreen.enabled = false;
-		startButton.enabled = true;
-		optionButton.enabled = true;
-		helpButton.enabled = true;
-		exitButton.enabled = true;
 	}
 
 	public void startGame()
 	{
-		Application.LoadLevel (1);
+		exitScreen.enabled = false;
+		optionScreen.enabled = false;
+		helpScreen.enabled = false;
 	}
 
 	public void exitGame()
